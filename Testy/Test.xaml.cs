c5db5b0753e76d27755c5aa0bb99bcfa5f99be8a
jAANUSZEK0700 <hpolkowski@wp.pl>
2015-06-12 13:12:56 +0200

    Wyświetlanie pytania.

--- Testy/Test.xaml.cs
+++ Testy/Test.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Testy
 {
@@ -19,9 +20,39 @@ namespace Testy
     /// </summary>
     public partial class Test : Window
     {
+        List<Question> data = new List<Question>();
+        List<Question> testQuestions = new List<Question>();
         public Test()
         {
             InitializeComponent();
+
+            XDocument xml = XDocument.Load("Questions.xml");
+            foreach (XElement element in xml.Root.Elements("Question"))
+            {
+                data.Add(new Question(
+                    int.Parse(element.Attribute("ID").Value.ToString()),
+                    element.Element("Text").Value.ToString(),
+                    element.Element("Answer1").Value.ToString(),
+                    element.Element("Answer2").Value.ToString(),
+                    element.Element("Answer3").Value.ToString(),
+                    element.Element("Answer4").Value.ToString()
+                    ));
+            }
+
+            //do zmiany. wybrać 10 losowych elementów z listy data
+            testQuestions.Add(data[1]);
+            //koniec losowania
+
+            //wyświetlanie pytania
+            Text_TextBlock.Text = testQuestions[0].Text;
+
+            //do zrobienia losowanie odpowiedzi
+
+            //wyświetlanie odpowiedzi
+            AnswerA.Content = testQuestions[0].Answer1;
+            AnswerB.Content = testQuestions[0].Answer2;
+            AnswerC.Content = testQuestions[0].Answer3;
+            AnswerD.Content = testQuestions[0].Answer4;
         }
     }
 }
